893ca7cee7dbd7004e36491f382cb045c6ea7c30
Daniel Kiss <littlesnorrboy@gmail.com>
2025-12-31 23:26:56 +0100

    Fix field name in debug

--- src/job_system.rs
+++ src/job_system.rs
@@ -873,7 +873,7 @@ impl<T: Clone> Clone for HomogeneousJobGraph<T> {
 impl<T: std::fmt::Debug> std::fmt::Debug for HomogeneousJobGraph<T> {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         f.debug_struct("HomogeneousJobGraph")
-            .field("data", &self.jobs)
+            .field("jobs", &self.jobs)
             .field("edges", &self.edges)
             .finish_non_exhaustive()
     }
